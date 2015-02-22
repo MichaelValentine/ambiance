d20ce1eb016207702ccc89f7c2b5e53d1ffb091b
Michael Valentine <Michael_Valentine@mentor.com>
2015-02-22 10:57:46 -0800

    Updating

--- dist/js/app.js
+++ dist/js/app.js
@@ -22,6 +22,12 @@
             $rootScope.$on("$stateChangeStart", function(event, to) {
             });
 
+            $rootScope.changeState=function(state){
+                $state.go(state);
+            };
+
+            $rootScope.friendList = ['Bob Burger', 'Sally Sue', 'Michel Valentine', "Yuriy Krushelnytskiy"];
+
             $rootScope.$state = $state;
             $rootScope.$stateParams = $stateParams;
 
@@ -48,10 +54,22 @@
                     templateUrl: "dist/partials/search.html",
                     cache: false
                 })
-                .state("meet-me", {
-                    controller: "meetMeCtrl",
-                    url: "/meet-me",
-                    templateUrl: "dist/partials/meet-me.html",
+                .state("meet", {
+                    controller: "meetCtrl",
+                    url: "/meet",
+                    templateUrl: "dist/partials/meet.html",
+                    cache: false
+                })
+                .state("meet.me", {
+                    controller: "meetCtrl",
+                    url: "/me",
+                    templateUrl: "dist/partials/meetMeForm.html",
+                    cache: false
+                })
+                .state("meet.me.view", {
+                    controller: "meetCtrl",
+                    url: "/view",
+                    templateUrl: "dist/partials/meetMeView.html",
                     cache: false
                 })
                 .state("searchby", {
@@ -82,7 +100,8 @@
         }
     ]);
 
-    angular.module("ambiance").controller("meetMeCtrl",  [
+
+    angular.module("ambiance").controller("meetCtrl",  [
         "$rootScope",
         "$state",
         "$timeout",
@@ -90,10 +109,50 @@
         "API",
         function($rootScope,$state,$timeout, $scope, API){
             // default globals
-            $rootScope.title = 'Meet Me';
+            $rootScope.title = 'Meet Up';
+
+            console.log($state);
+            if($state.is('meet.me.view')){
+                $scope.inviteList = angular.copy($scope.$parent.inviteList);
+                $scope.formData = angular.copy($scope.$parent.formData);
+            }
+            else if($state.is('meet')){
+
+            }
+            else{
+                $scope.friendList = angular.copy($rootScope.friendList);
+                $scope.inviteList = [];
+                $scope.formData = {};
+            }
+
+
+            $scope.importLocation= function(){
+                $scope.formData.location = "Henry's Tavern"
+            };
+
+            $scope.changeStateWithData = function(state){
+                $state.go(state, {location:false});
+            };
+
+            $scope.moveToList = function(i){
+                $scope.inviteList.push($scope.friendList.splice(i, 1)[0]);
+            };
+
+            $scope.removeFromList = function(i){
+                $scope.friendList.push($scope.inviteList.splice(i, 1)[0]);
+            };
+
+            $scope.validTime = function(){
+                return /^([0-1][0-9]|2[0-4]):[0-5][0-9]$/i.test($scope.formData.time);
+            };
+
+            $scope.validDate = function(){
+                console.log($scope.formData.date);
+                return /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/.test($scope.formData.date);
+            };
 
             $scope.changeState=function(state){
-                $state.go(state);
+                $state.go(state, {location:false});
             };
         }
     ]);
